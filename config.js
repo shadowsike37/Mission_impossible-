74fb62a2fcd6947d2e033d201b9d1b72ff290666
shadowsike37 <jinwoosung33833@gmail.com>
2026-02-06 00:01:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: " something special for you 💕 ",
+    pageTitle: "something special for you 💕",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
