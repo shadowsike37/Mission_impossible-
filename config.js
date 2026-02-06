f535f7589219921a876103f2ddeb01ba09ebdfbc
shadowsike37 <jinwoosung33833@gmail.com>
2026-02-06 09:32:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: " My angrybird 💖 ",
+    valentineName: " My pyari Bauni 💖 ",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
